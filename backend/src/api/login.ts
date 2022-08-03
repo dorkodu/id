f007bb9c9e00365d85b4b7c9aeb5881ec400d4d2
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-03 14:07:25 +0300

    Implement login

--- backend/src/api/login.ts
+++ backend/src/api/login.ts
@@ -1,6 +1,21 @@
-import { ApiCode, ApiReq } from "../../../shared/types";
+import { ApiCode, ApiError, ApiReq } from "../../../shared/types";
 import { ReqType, ResType } from "../../types";
 
-export async function login(req: ReqType, res: ResType, data: ApiReq[ApiCode.Login]) {
+import * as bcrypt from "bcrypt";
+import { validate } from "email-validator";
+import { DB } from "../db";
 
+export async function login(req: ReqType, res: ResType, data: ApiReq[ApiCode.Login]) {
+  if (!validate(data.email)) return res.send({ err: ApiError.SignupFail });
+  if (data.password.length < 10) return res.send({ err: ApiError.SignupFail });
+
+  const email = data.email;
+  const password = data.password;
+
+  const { result, err } = await DB.query(`SELECT password FROM user WHERE email=?`, [email]);
+
+  if (result.length === 0 || err) return res.send({ err: ApiError.LoginFail });
+  if (!(await bcrypt.compare(password, result[0].password))) return res.send({ err: ApiError.LoginFail });
+
+  return res.send({});
 }
