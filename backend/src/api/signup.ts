1943fd1bb7614d9d49636a9cf6ad62e8391a27b0
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-03 14:03:25 +0300

    Implement sign up

--- backend/src/api/signup.ts
+++ backend/src/api/signup.ts
@@ -1,6 +1,24 @@
-import { ApiCode, ApiReq } from "../../../shared/types";
+import { ApiCode, ApiError, ApiReq, ApiRes } from "../../../shared/types";
 import { ReqType, ResType } from "../../types";
 
-export async function signup(req: ReqType, res: ResType, data: ApiReq[ApiCode.Signup]) {
+import * as bcrypt from "bcrypt";
+import { validate } from "email-validator";
+import { DB } from "../db";
 
+export async function signup(req: ReqType, res: ResType, data: ApiReq[ApiCode.Signup]) {
+  if (!validate(data.email)) return res.send({ err: ApiError.SignupFail });
+  if (data.password.length < 10) return res.send({ err: ApiError.SignupFail });
+
+  const email = data.email;
+  const password = await bcrypt.hash(data.password, 10);
+  const date = Math.floor(Date.now() / 1000);
+
+  const { result, err } = await DB.query(`
+    INSERT INTO user  (email, password, date)
+    VALUES (?, ?, ?)
+  `, [email, password, date]);
+
+  if (err) return res.send({ err: ApiError.SignupFail });
+
+  return res.send({});
 }
