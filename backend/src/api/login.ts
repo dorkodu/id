ce20ca9af8847f8f0d44ad6c5781768c85cecbc1
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-04 12:27:55 +0300

    Fix login bugs & better tests

--- backend/src/api/login.ts
+++ backend/src/api/login.ts
@@ -4,7 +4,7 @@ import { ReqType, ResType } from "../../types";
 import * as bcrypt from "bcrypt";
 import { validate } from "email-validator";
 import { DB } from "../db";
-import { sha256 } from "../utilty";
+import { fromBinary, sha256 } from "../utilty";
 import { createToken } from "./token";
 
 export async function login(req: ReqType, res: ResType, data: ApiReq[ApiCode.Login]) {
@@ -14,12 +14,12 @@ export async function login(req: ReqType, res: ResType, data: ApiReq[ApiCode.Log
   const email = data.email;
   const password = data.password;
 
-  const { result, err } = await DB.query(`SELECT password FROM user WHERE email=?`, [email]);
+  const { result, err } = await DB.query(`SELECT id, password FROM user WHERE email=?`, [email]);
 
   if (result.length === 0 || err) return res.send({ err: ApiError.LoginFail });
-  if (!(await bcrypt.compare(sha256(password).toString("base64"), result[0].password))) return res.send({ err: ApiError.LoginFail });
+  if (!(await bcrypt.compare(sha256(password).toString("base64"), fromBinary(result[0].password, "utf8")))) return res.send({ err: ApiError.LoginFail });
 
-  const token = await createToken(result.insertId);
+  const token = await createToken(result[0].id);
   const redirectURI: string = (req.query as any)["redirect_uri"];
   res.redirect(`${redirectURI}?token=${token}`);
 }
