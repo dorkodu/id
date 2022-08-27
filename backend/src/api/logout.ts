a8fb37b97062ad8f6b8d7a6dbc1cf4759b8df10a
Berk Cambaz <berkcambaz12321@gmail.com>
2022-08-27 22:40:33 +0300

    Remove old api

--- backend/src/api/logout.ts
+++
@@ -1,9 +0,0 @@
-import { ApiCode, ApiReq } from "../../../shared/types";
-import { ReqType, ResType } from "../../types";
-import { deleteAuthToken } from "./auth";
-
-export async function logout(req: ReqType, res: ResType, data: ApiReq[ApiCode.Logout]) {
-  deleteAuthToken(data.token);
-
-  return res.send({ data: {} });
-}
