d90a56793fa37eef82ab70fdb45669f322538623
Berk Cambaz <berkcambaz12321@gmail.com>
2022-10-30 21:58:23 +0300

    Add stuff
    pg, config and cookie parser

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
+import { config } from "./config";
 
 async function main() {
   const app = express();
+
   app.use(express.json());
-  
-  app.listen(8001, () => { console.log(`Server has started on port ${8001}`) })
+  app.use(cookieParser());
+
+  app.listen(config.port, () => { console.log(`Server has started on port ${config.port}`) })
 }
 
 main();
