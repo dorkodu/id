83b32a5acaee7eeb0c16581a3385ca9e25a7cab8
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-01 10:56:34 +0300

    Implement changeUsername, changeEmail and changePassword

--- api/src/controllers/user.ts
+++ api/src/controllers/user.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { crypto } from "../lib/crypto";
 
 import pg from "../pg";
 import { changeEmailSchema, ChangePasswordSchema, changeUsernameSchema, getUserSchema } from "../schemas/user";
@@ -22,21 +23,60 @@ async function changeUsername(req: Request, res: Response) {
   const parsed = changeUsernameSchema.safeParse(req.body);
   if (!parsed.success) return void res.status(500).send();
 
-  const result = await pg``;
+  const info = auth.getAuthInfo(res);
+  if (!info) return void res.status(500).send();
+
+  const { newUsername } = parsed.data;
+  const [result]: [{ username: string }?] = await pg`
+    UPDATE users SET username=${newUsername} WHERE id=${info.userId} RETURNING username
+  `;
+  if (!result) return void res.status(500).send();
+  return void res.status(200).send();
 }
 
 async function changeEmail(req: Request, res: Response) {
   const parsed = changeEmailSchema.safeParse(req.body);
   if (!parsed.success) return void res.status(500).send();
 
-  const result = await pg``;
+  const info = auth.getAuthInfo(res);
+  if (!info) return void res.status(500).send();
+
+  const { newEmail, password } = parsed.data;
+
+  const [result0]: [{ password: Buffer }?] = await pg`
+    SELECT password FROM users WHERE id=${info.userId}
+  `;
+  if (!result0) return void res.status(500).send();
+  if (!crypto.comparePassword(password, result0.password)) return void res.status(500).send();
+
+  const [result1]: [{ email: string }?] = await pg`
+    UPDATE users SET email=${newEmail} WHERE id=${info.userId} RETURNING email
+  `;
+  if (!result1) return void res.status(500).send();
+  return void res.status(200).send();
 }
 
 async function changePassword(req: Request, res: Response) {
   const parsed = ChangePasswordSchema.safeParse(req.body);
   if (!parsed.success) return void res.status(500).send();
 
-  const result = await pg``;
+  const info = auth.getAuthInfo(res);
+  if (!info) return void res.status(500).send();
+
+  const oldPassword = parsed.data.oldPassword;
+  const newPassword = await crypto.encryptPassword(parsed.data.newPassword);
+
+  const [result0]: [{ password: Buffer }?] = await pg`
+    SELECT password FROM users WHERE id=${info.userId}
+  `;
+  if (!result0) return void res.status(500).send();
+  if (!crypto.comparePassword(oldPassword, result0.password)) return void res.status(500).send();
+
+  const [result1]: [{ password: string }?] = await pg`
+    UPDATE users SET password=${newPassword} WHERE id=${info.userId} RETURNING password
+  `;
+  if (!result1) return void res.status(500).send();
+  return void res.status(200).send();
 }
 
 export default {
