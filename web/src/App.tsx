37bbae19af8b1b8b719e4b2b30c5482b2a33deb6
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-01 13:17:12 +0300

    Implement a very basic ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,68 @@
+import { useState } from "react"
+
+interface State {
+  user: IUser | undefined;
+  authorized: boolean;
+}
+
+interface IUser {
+  id: number;
+  username: string;
+  email: string;
+  joinedAt: number;
+}
+
 function App() {
+  const [state, setState] = useState<State>({ user: undefined, authorized: false });
+
   return (
-    <div>
-      hello, world!
-    </div>
+    <>
+      {!state.authorized &&
+        <>
+          <div><input type={"text"} placeholder={"username or email..."} /></div>
+          <div><input type={"password"} placeholder={"password..."} /></div>
+          <button>login</button>
+          <br /><br />
+          <div><input type={"text"} placeholder={"username..."} /></div>
+          <div><input type={"email"} placeholder={"email..."} /></div>
+          <div><input type={"password"} placeholder={"password..."} /></div>
+          <button>signup</button>
+        </>
+      }
+      {state.authorized && state.user &&
+        <>
+          <div>username: {state.user.username}</div>
+          <div>email: {state.user.email}</div>
+          <div>joined at: {new Date(state.user.joinedAt * 1000).toString()}</div>
+          <br />
+
+          <div>
+            change username:
+            <div><input type={"text"} placeholder={"new username..."} /></div>
+            <button>apply</button>
+          </div>
+          <br />
+
+          <div>
+            change email:
+            <div><input type={"text"} placeholder={"new email..."} /></div>
+            <div><input type={"password"} placeholder={"password..."} /></div>
+            <button>apply</button>
+          </div>
+          <br />
+
+          <div>
+            change password:
+            <div><input type={"password"} placeholder={"old password..."} /></div>
+            <div><input type={"password"} placeholder={"new username..."} /></div>
+            <button>apply</button>
+          </div>
+          <br />
+
+          <button>logout</button>
+        </>
+      }
+    </>
   )
 }
 
