c564bef0975e7a6aa0891f37cd9c5fdc55ea290b
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-01 10:38:46 +0300

    Implement "getUser"

--- api/src/controllers/user.ts
+++ api/src/controllers/user.ts
@@ -2,12 +2,20 @@ import { Request, Response } from "express";
 
 import pg from "../pg";
 import { changeEmailSchema, ChangePasswordSchema, changeUsernameSchema, getUserSchema } from "../schemas/user";
+import auth from "./auth";
 
-async function getUser(req: Request, res: Response) {
+async function getUser(req: Request, res: Response<{ username: string, email: string, joinedAt: number }>) {
   const parsed = getUserSchema.safeParse(req.body);
   if (!parsed.success) return void res.status(500).send();
 
-  const result = await pg``;
+  const info = auth.getAuthInfo(res);
+  if (!info) return void res.status(500).send();
+
+  const [result]: [{ username: string, email: string, joinedAt: number }?] = await pg`
+    SELECT username, email, joinedAt FROM users WHERE id=${info.userId}
+  `;
+  if (!result) return void res.status(500).send();
+  return void res.status(200).send(result);
 }
 
 async function changeUsername(req: Request, res: Response) {
