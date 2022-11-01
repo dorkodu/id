5a5a8fe29c0b4fc4363b0b0322463b717c815177
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-01 12:51:29 +0300

    Basic setup for web

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa';
+import { createHtmlPlugin as html } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    html({ minify: true }),
+    viteCompression({ algorithm: "gzip" }),
+    viteCompression({ algorithm: "brotliCompress" }),
+    VitePWA({
+      devOptions: { enabled: true },
+      minify: true,
+      registerType: "prompt",
+      injectRegister: "inline",
+      workbox: {
+        globPatterns: ["**/*.{html,css,js,ico}"]
+      },
+      manifest: {}
+    })
   ],
   server: {
     watch: { usePolling: true },
