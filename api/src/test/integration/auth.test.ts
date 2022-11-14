9060bdccdb892df1e0b5217329ac6adfb3dc3b7c
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-14 15:22:16 +0300

    Implement first test for signup

--- api/src/test/integration/auth.test.ts
+++ api/src/test/integration/auth.test.ts
@@ -1,27 +1,45 @@
-import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { utils } from "./_utils";
 import axios from "axios";
-import { mailer } from "../../lib/mailer";
+import pg from "../../pg";
 
 describe("login", () => {
   beforeAll(async () => {
     await utils.resetDatabase();
   })
 
-  afterEach(() => {
-    vi.restoreAllMocks()
-  })
-
-  it("should initiate signup", async () => {
-    vi.spyOn(mailer, "sendConfirmEmail").mockImplementation((_email, _otp) => { console.log(_otp); return Promise.resolve(true); })
+  it("should signup", async () => {
+    const username = "test";
+    const email = "test@test.com";
+    const password = "12345678";
 
     await axios.post(
       "http://oath_api:8001/api/auth/initiateSignup",
-      { username: "test", email: "test@test.com" }
+      { username, email }
     )
       .then((_res => { }))
       .catch((_e) => { })
 
-    expect(500).toBe(500);
+    const [result0]: [{ otp: number }] = await pg`
+      SELECT otp FROM email_verification WHERE username=${username} AND email=${email}
+    `;
+    expect(result0).toBeDefined();
+    expect(result0.otp).toBeTypeOf("number");
+    expect(result0.otp).toBeGreaterThanOrEqual(100_000);
+    expect(result0.otp).toBeLessThan(1_000_000);
+
+    await axios.post(
+      "http://oath_api:8001/api/auth/confirmSignup",
+      { username, email, password, otp: result0.otp.toString() }
+    )
+      .then((_res => { }))
+      .catch((_e) => { })
+
+    const [result1]: [{ id: number }] = await pg`
+      SELECT id FROM users WHERE username=${username} AND email=${email}
+    `;
+
+    expect(result1).toBeDefined();
+    expect(result1.id).toBeTypeOf("number");
   })
 })
