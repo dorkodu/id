edee30d8c56e667f1bd7b4a767f3daaffdf36d20
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-16 20:28:20 +0300

    Update auth.test.ts

--- api/src/test/integration/auth.test.ts
+++ api/src/test/integration/auth.test.ts
@@ -1,7 +1,9 @@
-import { beforeAll, describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { utils } from "./_utils";
 import pg from "../../pg";
 import cookie from "cookie"
+import { request, response } from "express";
+import controller from "../../controllers/auth";
 
 async function signup(username: string, email: string, password: string) {
   await utils.request("initiateSignup", { username, email })
@@ -43,44 +45,38 @@ async function loginWithEmail(info: string, password: string) {
   return token;
 }
 
+const username = "test";
+const email = "test@test.com";
+const password = "12345678";
+
 describe("login", () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await utils.resetDatabase();
   })
 
   it("should signup", async () => {
-    const username = "test";
-    const email = "test@test.com";
-    const password = "12345678";
-
     const token = await signup(username, email, password);
 
     expect(token).toBeTypeOf("string");
   })
 
   it("should login with username", async () => {
-    const info = "test";
-    const password = "12345678";
-
-    const token = await loginWithUsername(info, password);
+    await signup(username, email, password);
+    const token = await loginWithUsername(username, password);
 
     expect(token).toBeTypeOf("string");
   })
 
   it("should login with email", async () => {
-    const info = "test@test.com";
-    const password = "12345678";
-
-    const token = await loginWithEmail(info, password);
+    await signup(username, email, password);
+    const token = await loginWithEmail(email, password);
 
     expect(token).toBeTypeOf("string");
   })
 
   it("should auth after login with username", async () => {
-    const info = "test";
-    const password = "12345678";
-
-    const token = await loginWithUsername(info, password);
+    await signup(username, email, password);
+    const token = await loginWithUsername(username, password);
     const cookies = cookie.serialize("token", token);
     const res = await utils.request("auth", {}, cookies);
 
@@ -88,10 +84,8 @@ describe("login", () => {
   })
 
   it("should auth after login with email", async () => {
-    const info = "test@test.com";
-    const password = "12345678";
-
-    const token = await loginWithEmail(info, password);
+    await signup(username, email, password);
+    const token = await loginWithEmail(username, password);
     const cookies = cookie.serialize("token", token);
     const res = await utils.request("auth", {}, cookies);
 
@@ -99,17 +93,16 @@ describe("login", () => {
   })
 
   it("should not signup with already used username/email", async () => {
-    const username = "test";
-    const email = "test@test.com";
+    await signup(username, email, password);
     const res = await utils.request("initiateSignup", { username, email })
 
     expect(res.status).toBe(500);
   })
 
   it("logout should work", async () => {
-    const info = "test";
-    const password = "12345678";
-    const token = await loginWithUsername(info, password);
+    await signup(username, email, password);
+
+    const token = await loginWithUsername(username, password);
     expect(token).toBeTypeOf("string");
 
     const cookies = cookie.serialize("token", token);
