ea9766a87e88d631a51305894f7fd2da4c0148ed
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-22 22:38:04 +0300

    Implement get accesses

--- api/src/controllers/access.ts
+++ api/src/controllers/access.ts
@@ -1,15 +1,32 @@
 import { Request, Response } from "express";
+import { IAccess } from "../../../shared/src/access";
+import { date } from "../lib/date";
+import pg from "../pg";
 
 import {
   AccessSchema, getAccessesSchema, grantAccessSchema,
   revokeAccessSchema
 } from "../schemas/access";
+import auth from "./auth";
 
 async function getAccesses(req: Request, res: Response<AccessSchema.OutputGetAccesses>): Promise<void> {
   const parsed = getAccessesSchema.safeParse(req.body);
   if (!parsed.success) return void res.status(500).send();
 
-  res.status(200).send();
+  const info = auth.getAuthInfo(res);
+  if (!info) return void res.status(500).send();
+
+  const { anchor, type } = parsed.data;
+  const result = await pg<IAccess[]>`
+    SELECT id, created_at, expires_at, user_agent, ip, service FROM access_tokens
+    WHERE user_id=${info.userId} AND expires_at>${date.utc()}
+    ${anchor === -1 ? pg`` : type === "newer" ? pg`AND id>${anchor}` : pg`AND id<${anchor}`}
+    ORDER BY id ${anchor === -1 ? pg`DESC` : type === "newer" ? pg`ASC` : pg`DESC`}
+    LIMIT 10
+  `;
+  if (!result.length) return void res.status(500).send();
+
+  return void res.status(200).send(result);
 }
 
 async function grantAccess(req: Request, res: Response<AccessSchema.OutputGrantAccess>): Promise<void> {
