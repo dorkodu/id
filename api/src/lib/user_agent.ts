d59347cde505295af789fa9ae6deb45726b80fb5
Berk Cambaz <berkcambaz12321@gmail.com>
2022-12-17 16:13:03 +0300

    Change user agent property order

--- api/src/lib/user_agent.ts
+++ api/src/lib/user_agent.ts
@@ -6,11 +6,15 @@ function parse(userAgent: string | undefined): string {
   const { browser, cpu, device, engine, os } = new UAParser(userAgent).getResult();
 
   const tags = [
-    browser.name, browser.version,
+    device.vendor,
+    device.model,
+    os.name,
+    os.version,
     cpu.architecture,
-    device.vendor, device.model,
-    engine.name, engine.version,
-    os.name, os.version,
+    browser.name,
+    browser.version,
+    engine.name,
+    engine.version,
   ].join(",");
 
   return tags.slice(0, 256);
