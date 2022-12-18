70b4b070a000af737e5d1136974081247b9f55df
Berk Cambaz <berkcambaz12321@gmail.com>
2022-12-18 23:50:30 +0300

    Add new env vars & handle env var numbers better

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,19 +1,24 @@
+function readNumber(variable: string | undefined) {
+  if (variable === undefined) return false;
+  return parseInt(variable);
+}
+
 const postgresHost = process.env.POSTGRES_HOST || "id_postgres";
-const postgresPort = (process.env.PGPORT && parseInt(process.env.PGPORT)) || 5432;
+const postgresPort = readNumber(process.env.PGPORT) || 5432;
 const postgresName = process.env.POSTGRES_DB || "id";
 const postgresUser = process.env.POSTGRES_USER || "postgres";
 const postgresPassword = process.env.POSTGRES_PASSWORD || "postgres";
 
 const smtpHost = process.env.SMTP_HOST || "id_mailslurper";
-const smtpPort = (process.env.SMTP_PORT && parseInt(process.env.SMTP_PORT)) || 2500;
+const smtpPort = readNumber(process.env.SMTP_PORT) || 2500;
 const smtpUser = process.env.SMTP_USER || "";;
 const smtpPassword = process.env.SMTP_PASSWORD || "";;
 
-const bcryptRounds = (process.env.BCRYPT_ROUNDS && parseInt(process.env.BCRYPT_ROUNDS)) || 10;
-const epochTime = (process.env.EPOCH_TIME && parseInt(process.env.EPOCH_TIME)) || 1671366107647;
-const machineId = (process.env.MACHINE_ID && parseInt(process.env.MACHINE_ID)) || 0;
+const bcryptRounds = readNumber(process.env.BCRYPT_ROUNDS) || 10;
+const epochTime = readNumber(process.env.EPOCH_TIME) || 1671366107647;
+const machineId = readNumber(process.env.MACHINE_ID) || 0;
 
-const port = (process.env.PORT && parseInt(process.env.PORT)) || 8001;
+const port = readNumber(process.env.PORT) || 8001;
 const env = process.env.NODE_ENV || "development";
 
 const serviceWhitelist = (process.env.SERVICE_WHITELIST && process.env.SERVICE_WHITELIST.split(",")) || [
