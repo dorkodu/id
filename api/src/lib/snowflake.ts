c2570bb630e1f79c2c267bbd47965d357c80abf7
Berk Cambaz <berkcambaz12321@gmail.com>
2022-12-19 12:02:53 +0300

    Finish snowflake implementation

--- api/src/lib/snowflake.ts
+++ api/src/lib/snowflake.ts
@@ -1,28 +1,44 @@
 import { config } from "../config";
+import Long from "long";
 
-class SnowflakeGenerator {
+class Snowflake {
   public static readonly epochTime: number = config.epochTime;
   public static readonly machineId: number = config.machineId;
 
+  public static readonly machineIdBits = 10;
+  public static readonly sequenceIdBits = 12;
+
+  public static readonly maxMachineId = Math.pow(2, Snowflake.machineIdBits);
+  public static readonly maxSequenceId = Math.pow(2, Snowflake.sequenceIdBits);
+
+  public static readonly machineIdShift = Snowflake.sequenceIdBits;
+  public static readonly timestampShift = Snowflake.sequenceIdBits + Snowflake.machineIdBits;
+
   public lastTimestamp: number = -1;
 
-  public timestamp: number;
-  public sequenceId: number;
+  public timestamp: number = Date.now();
+  public sequenceId: number = 0;
 
   constructor() {
-    this.timestamp = Date.now();
-    this.sequenceId = 0;
+    if (Snowflake.machineId >= Snowflake.maxMachineId) {
+      throw "Machine id can't be bigger than or equal to max machine id."
+    }
+
+    if (Snowflake.machineId < 0) {
+      throw "Machine id can't be less than 0."
+    }
   }
 
   public id() {
     this.timestamp = Date.now();
 
+    // If clock somehow went back in time
     if (this.timestamp < this.lastTimestamp) {
       this.timestamp = this.lastTimestamp;
     }
 
     if (this.timestamp === this.lastTimestamp) {
-      this.sequenceId = this.sequenceId + 1;
+      this.sequenceId = (this.sequenceId + 1) % Snowflake.maxSequenceId;
       if (this.sequenceId === 0) this.timestamp++;
     }
     else {
@@ -31,26 +47,26 @@ class SnowflakeGenerator {
 
     this.lastTimestamp = this.timestamp;
 
-    return {
-      timestamp: this.timestamp,
-      machine: SnowflakeGenerator.machineId,
-      sequence: this.sequenceId
-    };
+    const timestamp = Long.fromNumber(this.timestamp - Snowflake.epochTime).shiftLeft(Snowflake.timestampShift);
+    const machineId = Long.fromNumber(Snowflake.machineId).shiftLeft(Snowflake.machineIdShift);
+    const sequenceId = Long.fromNumber(this.sequenceId);
+
+    return timestamp.add(machineId).add(sequenceId).toString();
   }
 }
 
 const generators = {
-  "users": new SnowflakeGenerator(),
-  "sessions": new SnowflakeGenerator(),
+  "users": new Snowflake(),
+  "sessions": new Snowflake(),
 
-  "email_new_location": new SnowflakeGenerator(),
-  "email_verify_email": new SnowflakeGenerator(),
-  "email_confirm_email": new SnowflakeGenerator(),
-  "email_revert_email": new SnowflakeGenerator(),
-  "email_change_password": new SnowflakeGenerator(),
+  "email_new_location": new Snowflake(),
+  "email_verify_email": new Snowflake(),
+  "email_confirm_email": new Snowflake(),
+  "email_revert_email": new Snowflake(),
+  "email_change_password": new Snowflake(),
 
-  "access_tokens": new SnowflakeGenerator(),
-  "access_codes": new SnowflakeGenerator(),
+  "access_tokens": new Snowflake(),
+  "access_codes": new Snowflake(),
 }
 
 function id(generator: keyof typeof generators) {
