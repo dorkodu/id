c477836caa22343dd70a0945549fb2116dcc4df4
doruk eray <doruk@dorkodu.com>
2023-01-15 02:55:58 +0300

    (web) add color scheme status to appStore

--- web/src/stores/appStore.ts
+++ web/src/stores/appStore.ts
@@ -1,13 +1,15 @@
-import create from "zustand"
-import { immer } from 'zustand/middleware/immer'
-import { changeDateLanguage } from "../lib/date"
-import i18n from "../lib/i18n"
+import { ColorScheme } from "@mantine/core";
+import create from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { changeDateLanguage } from "../lib/date";
+import i18n from "../lib/i18n";
 
 interface State {
   loading: {
-    auth: boolean
-    locale: boolean
-  }
+    auth: boolean;
+    locale: boolean;
+  };
+  colorScheme: ColorScheme;
 }
 
 interface Action {
@@ -15,36 +17,54 @@ interface Action {
   setAuthLoading: (loading: boolean) => void;
   setLocaleLoading: (loading: boolean) => void;
   changeLocale: (lang: string) => void;
+  toggleColorScheme: (value: ColorScheme) => void;
 }
 
 const initialState: State = {
   loading: {
     auth: true,
     locale: true,
-  }
-}
-
-export const useAppStore = create(immer<State & Action>((set, get) => ({
-  ...initialState,
-
-  getLoading: () => {
-    return get().loading.auth || get().loading.locale;
   },
+  colorScheme: "light",
+};
 
-  setAuthLoading: (loading) => {
-    set(state => { state.loading.auth = loading })
-  },
+export const useAppStore = create(
+  immer<State & Action>((set, get) => ({
+    ...initialState,
 
-  setLocaleLoading: (loading) => {
-    set(state => { state.loading.locale = loading })
-  },
+    toggleColorScheme(value) {
+      set((state) => {
+        state.colorScheme = value;
+      });
+    },
 
-  changeLocale: async (lang) => {
-    set(state => { state.loading.locale = true })
+    getLoading: () => {
+      return get().loading.auth || get().loading.locale;
+    },
 
-    await Promise.all([i18n.changeLanguage(lang), changeDateLanguage(lang)])
-    document.documentElement.lang = lang;
+    setAuthLoading: (loading) => {
+      set((state) => {
+        state.loading.auth = loading;
+      });
+    },
 
-    set(state => { state.loading.locale = false })
-  }
-})))
+    setLocaleLoading: (loading) => {
+      set((state) => {
+        state.loading.locale = loading;
+      });
+    },
+
+    changeLocale: async (lang) => {
+      set((state) => {
+        state.loading.locale = true;
+      });
+
+      await Promise.all([i18n.changeLanguage(lang), changeDateLanguage(lang)]);
+      document.documentElement.lang = lang;
+
+      set((state) => {
+        state.loading.locale = false;
+      });
+    },
+  }))
+);
