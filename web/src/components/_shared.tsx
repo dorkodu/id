a3fc48bee37513bc205b0e18a4e3265d60871cfa
doruk eray <doruk@dorkodu.com>
2023-01-14 22:05:39 +0300

    (web) add Header and Footer for FormPage

--- web/src/components/_shared.tsx
+++ web/src/components/_shared.tsx
@@ -6,4 +6,39 @@ import { ReactComponent as DorkoduLogo } from "@assets/dorkodu_Logo.svg";
 import DorkoduIDKeyIcon from "@assets/dorkodu-id_key.svg";
 
 export const FormPage = {
+  Header: () => (
+    <>
+      <Image
+        src={DorkoduIDKeyIcon}
+        width={100}
+        sx={{
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      />
+    </>
+  ),
+  Footer: () => (
+    <FooterSimple
+      links={[
+        {
+          link: "https://dorkodu.com",
+          label: "About",
+        },
+        {
+          link: "https://dorkodu.com/privacy",
+          label: "Privacy",
+        },
+        {
+          link: "https://garden.dorkodu.com",
+          label: "Garden",
+        },
+        {
+          link: "https://dorkodu.com/work",
+          label: "Work",
+        },
+      ]}
+      brand={<DorkoduLogo width={160} height={40} />}
+    />
+  ),
 };
