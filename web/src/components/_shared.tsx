d7e89253bf82c5478ce28666d8bc0fa2581f8cbe
doruk eray <doruk@dorkodu.com>
2023-01-14 22:05:13 +0300

    (web) create file: _shared.tsx
    for shared, data-fed, ready to use view elements

---
+++ web/src/components/_shared.tsx
@@ -0,0 +1,9 @@
+import { Image } from "@mantine/core";
+
+import { FooterSimple } from "./Footer";
+
+import { ReactComponent as DorkoduLogo } from "@assets/dorkodu_Logo.svg";
+import DorkoduIDKeyIcon from "@assets/dorkodu-id_key.svg";
+
+export const FormPage = {
+};
