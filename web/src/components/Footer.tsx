7e5eca912cef691eba3754e4a9ab894b391b9a7b
doruk eray <doruk@dorkodu.com>
2023-01-14 22:03:13 +0300

    (web) create component collection: Footer

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,60 @@
+import {
+  createStyles,
+  Container,
+  Group,
+  Anchor,
+  Space,
+  Box,
+} from "@mantine/core";
+import { FunctionComponent } from "react";
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: 16,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[3]
+    }`,
+  },
+
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+}));
+
+export const FooterSimple: FunctionComponent<{
+  links: { link: string; label: string }[];
+  brand: React.ReactNode;
+}> = ({ links, brand }) => {
+  const { classes } = useStyles();
+
+  const items = links.map((link) => (
+    <Anchor<"a">
+      color="dimmed"
+      key={link.label}
+      href={link.link}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Group>{items}</Group>
+        <Space h={20} />
+        <Box>{brand}</Box>
+      </Container>
+    </div>
+  );
+};
