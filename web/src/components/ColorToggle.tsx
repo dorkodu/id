ced0ab5e04cc7b7c0501f1ffedf5a4d6fd93f01c
doruk eray <doruk@dorkodu.com>
2023-01-15 02:51:26 +0300

    (web) new component collection: ColorToggle

---
+++ web/src/components/ColorToggle.tsx
@@ -0,0 +1,72 @@
+import { useTheme } from "@emotion/react";
+import {
+  SegmentedControl,
+  Group,
+  Center,
+  ActionIcon,
+  useMantineColorScheme,
+} from "@mantine/core";
+
+import { IconSun, IconMoon, IconMoonStars } from "@tabler/icons";
+
+export function ColorToggleSegmented() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const theme = useTheme();
+
+  return (
+    <Group position="center" my="xl">
+      <SegmentedControl
+        radius="md"
+        value={colorScheme}
+        onChange={(value: "light" | "dark") => {
+          toggleColorScheme(value);
+        }}
+        data={[
+          {
+            value: "light",
+            label: (
+              <Center>
+                <IconSun size={18} stroke={3} />
+              </Center>
+            ),
+          },
+          {
+            value: "dark",
+            label: (
+              <Center>
+                <IconMoon size={18} stroke={3} />
+              </Center>
+            ),
+          },
+        ]}
+      />
+    </Group>
+  );
+}
+
+export function ColorToggleActionIcon() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  return (
+    <ActionIcon
+      onClick={() => toggleColorScheme()}
+      size="lg"
+      sx={(theme) => ({
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color:
+          theme.colorScheme === "dark"
+            ? theme.colors.yellow[4]
+            : theme.colors.blue[6],
+      })}
+    >
+      {colorScheme === "dark" ? (
+        <IconSun size={18} />
+      ) : (
+        <IconMoonStars size={18} />
+      )}
+    </ActionIcon>
+  );
+}
