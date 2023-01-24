0bb1c2536c87c93456e69bb5e37b4606c9eef812
doruk eray <doruk@dorkodu.com>
2023-01-24 17:04:50 +0300

    (web) implement component: UserDashboardProfile

--- web/src/components/User.tsx
+++ web/src/components/User.tsx
@@ -26,3 +26,53 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+export const UserDashboardProfile: FunctionComponent<{
+  data?: {
+    name?: string;
+    username?: string;
+    email?: string;
+    avatar?: string;
+    bio?: string;
+    joinedAt?: number;
+  };
+}> = ({ data }) => {
+  const { classes: styles } = useStyles();
+
+  if (!data)
+    return (
+      <Alert icon={<IconExclamationMark size={32} />} title="Oops!" color="red">
+        Failed to load user.
+      </Alert>
+    );
+
+  return (
+    <Paper shadow="xs" p="md" maw={600}>
+      <Group>
+        <Avatar src={data.avatar} size={94} radius="md" />
+        <Stack spacing={2}>
+          <Text size="xl" weight={600}>
+            {data.username}
+          </Text>
+
+          <Text size="md" weight={500}>
+            {data.name}
+          </Text>
+
+          <Group noWrap spacing={10} mt={3} align="center">
+            <IconMailOpened size={24} className={styles.icon} />
+            <Text size="sm" color="dimmed">
+              {data.email}
+            </Text>
+          </Group>
+
+          <Group noWrap spacing={10} mt={5} align="center">
+            <IconCalendar size={24} className={styles.icon} />
+            <Text size="sm" color="dimmed">
+              {data.joinedAt && date(data.joinedAt).format("lll")}
+            </Text>
+          </Group>
+        </Stack>
+      </Group>
+    </Paper>
+  );
+};
