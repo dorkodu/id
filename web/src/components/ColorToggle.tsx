53a18976dddfaa20328ec18df2c389ad152e2395
doruk eray <doruk@dorkodu.com>
2023-01-25 20:41:27 +0300

    (web) redesign segmented color toggle

--- web/src/components/ColorToggle.tsx
+++ web/src/components/ColorToggle.tsx
@@ -12,33 +12,31 @@ export function ColorToggleSegmented() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
-    <Group position="center" my="xl">
-      <SegmentedControl
-        radius="md"
-        value={colorScheme}
-        onChange={(value: "light" | "dark") => {
-          toggleColorScheme(value);
-        }}
-        data={[
-          {
-            value: "light",
-            label: (
-              <Center>
-                <IconSun size={18} stroke={3} />
-              </Center>
-            ),
-          },
-          {
-            value: "dark",
-            label: (
-              <Center>
-                <IconMoon size={18} stroke={3} />
-              </Center>
-            ),
-          },
-        ]}
-      />
-    </Group>
+    <SegmentedControl
+      radius="md"
+      value={colorScheme}
+      onChange={(value: "light" | "dark") => {
+        toggleColorScheme(value);
+      }}
+      data={[
+        {
+          value: "light",
+          label: (
+            <Center>
+              <IconSun size={18} stroke={3} />
+            </Center>
+          ),
+        },
+        {
+          value: "dark",
+          label: (
+            <Center>
+              <IconMoon size={18} stroke={3} />
+            </Center>
+          ),
+        },
+      ]}
+    />
   );
 }
 
