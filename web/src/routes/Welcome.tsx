1932c2e61624abb2800ce6d629ad3dcdadde6617
doruk eray <doruk@dorkodu.com>
2023-01-25 17:52:57 +0300

    (web) update route: Welcome
    no longer redirect to dashboard if already has login.

--- web/src/routes/Welcome.tsx
+++ web/src/routes/Welcome.tsx
@@ -37,109 +37,119 @@ function Welcome() {
 
   const authorized = useUserStore((state) => state.authorized);
 
+  const AuthOptions = () => (
+    <Stack>
+      <Button
+        variant="filled"
+        onClick={() => {
+          navigate("/create-account");
+        }}
+        radius="md">
+        Create Account
+      </Button>
+      <Button
+        variant="default"
+        onClick={() => {
+          navigate("/login");
+        }}
+        radius="md">
+        Log In
+      </Button>
+
+      <Anchor
+        color="blue"
+        size={15}
+        weight={450}
+        onClick={(e) => {
+          e.preventDefault();
+          navigate("/change-password");
+        }}
+        align="center"
+        mt={10}>
+        <Box ml={5}>Forgot your password?</Box>
+      </Anchor>
+    </Stack>
+  );
+
   return (
-    <>
-      {authorized && navigate("/dashboard")}
-      {!authorized && (
-        <Container size={460} my={25}>
-          <FormPage.Header />
+    <Container size={460} my={25}>
+      <FormPage.Header />
 
-          <Title order={1} size={32} align="center" mb={5}>
-            Your Digital Life,
-            <br />
-            One Account.
-          </Title>
+      <Title order={1} size={32} align="center" mb={5}>
+        Your Digital Life,
+        <br />
+        One Account.
+      </Title>
 
-          <Text
-            color="dimmed"
-            size="lg"
-            align="center"
-            weight={600}
-            mt="sm"
-            maw={320}
-            mx="auto">
-            Connect with ID, and get the most out of all the Dorkodu apps you
-            use.
-          </Text>
+      <Text
+        color="dimmed"
+        size="lg"
+        align="center"
+        weight={600}
+        mt="sm"
+        maw={320}
+        mx="auto">
+        Connect with ID, and get the most out of all the Dorkodu apps you use.
+      </Text>
 
-          <Paper p={30} radius="lg" my="md" maw={300} mx="auto">
-            <Stack>
-              <Button
-                variant="filled"
-                onClick={() => {
-                  navigate("/create-account");
-                }}
-                radius="md">
-                Create Account
-              </Button>
-              <Button
-                variant="default"
-                onClick={() => {
-                  navigate("/login");
-                }}
-                radius="md">
-                Log In
-              </Button>
+      <Paper p={30} radius="lg" my="md" maw={300} mx="auto">
+        {authorized && (
+          <Stack>
+            <Button
+              variant="filled"
+              onClick={() => {
+                navigate("/dashboard");
+              }}
+              radius="md">
+              Continue to Dashboard
+            </Button>
+          </Stack>
+        )}
+        {!authorized && <AuthOptions />}
+      </Paper>
 
-              <Anchor
-                color="blue"
-                size={15}
-                weight={450}
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate("/change-password");
-                }}
-                align="center"
-                mt={10}>
-                <Box ml={5}>Forgot your password?</Box>
-              </Anchor>
-            </Stack>
-          </Paper>
+      <Center>
+        <List my="lg" spacing="md" mx="auto" center>
+          <List.Item
+            icon={
+              <ThemeIcon {...styles.themeIcons} color="cyan">
+                <IconUser />
+              </ThemeIcon>
+            }>
+            Get a personalized experience.
+          </List.Item>
 
-          <Center>
-            <List my="lg" spacing="md" mx="auto" center>
-              <List.Item
-                icon={
-                  <ThemeIcon {...styles.themeIcons} color="cyan">
-                    <IconUser />
-                  </ThemeIcon>
-                }>
-                Get a personalized experience.
-              </List.Item>
+          <List.Item
+            icon={
+              <ThemeIcon {...styles.themeIcons} color="blue">
+                <IconDiscountCheck />
+              </ThemeIcon>
+            }>
+            Be verified everywhere.
+          </List.Item>
 
-              <List.Item
-                icon={
-                  <ThemeIcon {...styles.themeIcons} color="blue">
-                    <IconDiscountCheck />
-                  </ThemeIcon>
-                }>
-                Be verified everywhere.
-              </List.Item>
+          <List.Item
+            icon={
+              <ThemeIcon {...styles.themeIcons} color="indigo">
+                <IconLock />
+              </ThemeIcon>
+            }>
+            Your life's information in one place.
+          </List.Item>
 
-              <List.Item
-                icon={
-                  <ThemeIcon {...styles.themeIcons} color="indigo">
-                    <IconLock />
-                  </ThemeIcon>
-                }>
-                Your life's information in one place.
-              </List.Item>
+          <List.Item
+            icon={
+              <ThemeIcon {...styles.themeIcons} color="violet">
+                <IconUnlink />
+              </ThemeIcon>
+            }>
+            All apps are always connected.
+          </List.Item>
+        </List>
+      </Center>
 
-              <List.Item
-                icon={
-                  <ThemeIcon {...styles.themeIcons} color="violet">
-                    <IconUnlink />
-                  </ThemeIcon>
-                }>
-                All apps are always connected.
-              </List.Item>
-            </List>
-          </Center>
-
-          <FormPage.Footer />
-        </Container>
-      )}
-    </>
+      <FormPage.Footer />
+    </Container>
   );
 }
 
