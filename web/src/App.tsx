66ed84d55179d6e04020853411f03c7e7e0970b0
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-30 00:37:12 +0300

    Load rubik regular font

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,7 @@ import { Outlet } from "react-router-dom";
 import { useAppStore } from "./stores/appStore";
 import { useUserStore } from "./stores/userStore";
 import theme from "./styles/theme";
+import RubikRegular from "@assets/fonts/Rubik-Regular.woff2";
 
 const width = css`
   max-width: 768px;
@@ -15,6 +16,11 @@ const global = css`
   body {
     ${width}
     overflow-y: scroll;
+  }
+  
+  @font-face {
+    font-family: Rubik;
+    src: url(${RubikRegular}) format("woff2");
   }
 `;
 
