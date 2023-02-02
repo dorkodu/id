d5097051e4aa4de9dbc989438285d316bcd7fec0
doruk eray <doruk@dorkodu.com>
2023-02-02 15:12:37 +0300

    implement StandardEmailMessage

--- api/src/lib/mail/template.ts
+++ api/src/lib/mail/template.ts
@@ -8,4 +8,46 @@ import { Block } from "./mailscript";
 type EmailBlueprint<T extends { address: string }> = (data: T) => Mail.Options;
 
 export namespace EmailTemplate {
+  const StandardEmailMessage = ({
+    to,
+    subject,
+    summary,
+    text,
+    html,
+  }: {
+    to: string;
+    subject: string;
+    summary?: string;
+    text: string;
+    html: string;
+  }) => {
+    return {
+      from: '"Dorkodu ID" <hey@dorkodu.com>',
+      to,
+      subject: subject,
+      text: `
+  ${subject}
+  
+  ${summary ?? ""}
+  
+  ${text}
+  
+  © 2023 Dorkodu 
+  <https://dorkodu.com>
+  `,
+      html: Block.Document(
+        subject,
+        summary ?? "",
+        `${Block.Card(`
+          ${Block.Logo(
+            "https://id.dorkodu.com/images/dorkodu-id.svg",
+            "Dorkodu ID"
+          )}
+          ${Block.Title(subject)}
+          ${html}
+        `)} 
+        ${Block.Footer()}`
+      ),
+    } satisfies Mail.Options;
+  };
 }
