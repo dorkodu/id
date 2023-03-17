b9ad427491da1bd7d59bcc95e202d7f674d4d014
Berk Cambaz <berkcambaz12321@gmail.com>
2023-03-17 17:52:23 +0300

    More stuff towards next

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from "@mantine/core"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
